d44f82d8d855959cf1727fdaa163335a2cf6650c
maninserg <manin.serg@gmail.com>
2021-02-08 14:13:10 +0300

    URL routing in Flask

--- headlines.py
+++ headlines.py
@@ -4,20 +4,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-BBC_FEED = "https://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {"bbc": "https://feeds.bbci.co.uk/news/rss.xml",
+             "cnn": "http://rss.cnn.com/rss/edition.rss",
+             "fox": "http://feeds.foxnews.com/foxnews/latest",
+             "fontanka": "https://www.fontanka.ru/fontanka.rss",
+             "iol": "https://www.iol.co.za/cmlink/1.640"}
 
 
 @app.route("/")
-def get_news():
-    feed = feedparser.parse(BBC_FEED)
-    first_article = feed['entries'][0]
+@app.route("/<publication>")
+def get_news(publication="bbc"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article = feed["entries"][0]
     return """<html>
-    <body>
-    <h1> BBC Headlines </h1>
-    <b>{0}</b> <br/>
-    <i>{1}</i> <br/>
-    <p>{2}</p> <br/>
-    <body>
+        <body>
+            <h1>Headlines</h1>
+            <b>{0}</b><br>
+            <i>{1}</b><br>
+            <p>{2}</p><br>
+        </body>
     </html>""".format(first_article.get("title"),
                       first_article.get("published"),
                       first_article.get("summary"))
